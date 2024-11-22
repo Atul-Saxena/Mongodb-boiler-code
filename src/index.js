9598a4dca2c1da7aa8e65a9e251fe05cc3d52a0b
Atul Saxena <atulsaxena194@gmail.com>
2024-11-22 15:38:38 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import 'dotenv/config'
 import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB().then(() => {
+    console.log("Connection successful !!!");
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙ Server running on port ${process.env.PORT}`);
+    })
+}).catch((error) => {
+    console.log("Connection failed!",error);
+});
 
 
